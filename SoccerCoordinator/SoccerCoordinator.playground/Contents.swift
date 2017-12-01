a1389821d21f99dc7c64a9d1c1eaed4e35ad309a
Connor hoare <connorhoare@Connors-MacBook-Pro.local>
2017-12-01 09:56:38 +0000

    Added list of players into the collection

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,9 +1,23 @@
-//: Playground - noun: a place where people can play
+// Creating the list of players
 
-import UIKit
+var playerList = [
+    "Player 1": "Joe Smith, 42 inches, Has Experience, Jim and Jan Smith",
+    "Player 2": "Jill Tanner, 36 inches, Has Experience, Clara Tanner",
+    "Player 3": "Bill Bon, 43 inches, Has Experience, Sara and Jenny Bon",
+    "Player 4": "Eva Gordon, 45 inches, No Experience, Wendy and Mike Gordon",
+    "Player 5": "Matt Gil, 40 inches, no experience, Charles and Sylvia Gill",
+    "Player 6": "Kimmy Stein, 41 inches, no experience, Bill and Hilary Stein",
+    "Player 7": "Sammy Adams, 45 inches, no experience, Jeff Adams",
+    "Player 8": "Karl Saygan, 42 inches, Has experience, Heather Bledsoe",
+    "Player 9": "Suzane Greenberg, 44 inches, Has experience, Henrietta Dumas",
+    "Player 10": "Sal Dali, 41 inches, no experience, Gala Dali",
+    "Player 11": "Joe Kavalier, 39 inches, no experience, Sam and Elaine Kavalier",
+    "Player 12": "Ben Finkelstein, 44 inches, no experience, Aaron and Jill Finkelstein",
+    "Player 13": "Diego Soto, 41 inches, Has experience, Robin and Sarika Soto",
+    "Player 14": "Chloe Alaska, 47 inches, no experience, David and Jamie Alaska",
+    "Player 15": "Arnold Willis, 43 inches, no experience, Claire Willis",
+    "Player 16": "Phillip Helm, 44 inches, Has experience, Thomas Helm and Eva Jones",
+    "Player 17": "Les Clay, 42 inches, Has experience, Wynonna Brown",
+    "Player 18": "Herschel Krustofski, 45 inches, Has experience, Hyman and Rachel Krustofski"
+]
 
-var str = "Hello, playground"
-
-// Connor playground
-
-// This is my playground
